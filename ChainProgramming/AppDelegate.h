99597967e08bac12b295591f040dd1f4eba48779
chengxuzheng <zheng890715@gmail.com>
2017-03-30 10:33:34 +0800

    Initial Commit

---
+++ ChainProgramming/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChainProgramming
+//
+//  Created by Zheng on 2017/3/30.
+//  Copyright © 2017年 Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
